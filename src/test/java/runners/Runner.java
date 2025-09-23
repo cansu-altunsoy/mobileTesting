c770d7c41cc25bde085181f3093d15da9311c9a2
Cansu Altunsoy <cansualtunsoy@gmail.com>
2025-09-23 17:30:04 +0300

    run1

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,35 @@
+package runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty",
+                "json:target/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        features = "src/test/resources/Features",
+        glue = "stepDefinitions",
+        tags = "@E2E" ,
+        dryRun = false
+
+
+)
+public class Runner {
+
+    private static final Logger logger = LogManager.getLogger(Runner.class);
+
+
+    @BeforeClass
+    public static void setup() {
+
+        // Test çalışmaya başlamadan önce loglama yapılır
+        logger.info("Cucumber Test Runner başlatılıyor...");
+
+    }
+
+}
