d654dc074c65f44b6172db4af5c7b90b3744d114
Cansu Altunsoy <cansualtunsoy@gmail.com>
2025-10-24 21:12:53 +0300

    stepPage

--- src/main/java/pages/MyStepPage.java
+++ src/main/java/pages/MyStepPage.java
@@ -59,14 +59,11 @@ public class MyStepPage {
     @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(0)")
     public WebElement wishlistComeWarningMessage;
 
-
     @AndroidFindBy(xpath = "(//android.view.View)[19]")
     public WebElement relevantProduct;
 
-
     @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Favorite\")")
     public WebElement relevantFavorite;
-
 
     @AndroidFindBy(uiAutomator = "new UiSelector().description(\"Women\")")
     public WebElement homepageScrollWomenCategory;
